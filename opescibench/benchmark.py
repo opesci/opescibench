acfee1e00999a4ddd6301d9ec53b2e00bcc7404b
Michael Lange <michael.lange@imperial.ac.uk>
2016-05-27 09:52:58 +0100

    Adding parameter sweep utilities to `Benchmark`

--- opescibench/benchmark.py
+++ opescibench/benchmark.py
@@ -1,4 +1,6 @@
 from opescibench.argparser import ArgParser
+from collections import OrderedDict
+from itertools import product
 
 __all__ = ['Benchmark']
 
@@ -10,16 +12,33 @@ class Benchmark(object):
         self.parser = ArgParser(**kwargs)
 
         self._params = []
-        self._paramvalues = []
+        self._values = {}
 
     def add_parameter(self, name, *parserargs, **parserkwargs):
         self._params += [name.lstrip('-')]
         self.parser.add_parameter(name, *parserargs, **parserkwargs)
 
-    def add_parameter_values(self, name, values):
+    def add_parameter_value(self, name, values):
         self._params += [name]
-        self._paramvalues += [values]
+        self._values[name] = values
 
     @property
     def params(self):
+        """ Lexicographically sorted parameter key """
         return tuple(sorted(self._params))
+
+    @property
+    def values(self):
+        """ Sorted dict of parameter-value mappings """
+        for p in self.params:
+            if hasattr(self.parser.args, p):
+                self._values[p] = getattr(self.parser.args, p)
+        # Ensure all values are lists
+        valuelist = [(k, [v]) if not isinstance(v, list) else (k, v)
+                     for k, v in self._values.items()]
+        return OrderedDict(sorted(valuelist))
+
+    @property
+    def sweep(self):
+        """ List of value mappings for each instance of a parameter sweep. """
+        return [OrderedDict(zip(self.params, v)) for v in product(*self.values.values())]
