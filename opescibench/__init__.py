d9672927c7b5fc04ba8682cb8531cadd89377cf9
Michael Lange <michael.lange@imperial.ac.uk>
2017-11-28 13:53:37 +0000

    Setup: Add better auto-versioning via `setuptools_rcm`

--- opescibench/__init__.py
+++ opescibench/__init__.py
@@ -1,3 +1,10 @@
 from opescibench.benchmark import *
 from opescibench.executor import *
 from opescibench.plotter import *
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
