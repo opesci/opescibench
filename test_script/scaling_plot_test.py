9b56f0d6f43a3b0c529d2387b1c6d5533fdbcc14
George Bisbas <g.bisbas18@imperial.ac.uk>
2019-10-02 15:21:27 +0100

    plot_test.py Some additions

--- test_script/scaling_plot_test.py
+++ test_script/scaling_plot_test.py
@@ -33,24 +33,35 @@ def extract_xy(files, path, var='gflopss', _ordered=True):
         xvals, yvals = (list(i) for i in zip(*sorted(zip(xvals, yvals))))
     return xvals, yvals
 
+
+# Rank of interest
+rank = 'rank[0]'
+
 path = '/data/cx2_data/benchmarks/elastic/results/'
 files = scan_dir(path)
-
-rank = 'rank[0]'
 files = [i for i in files if rank in i]
-
 x0, y0 = extract_xy(files, path)
 
 path = '/data/cx2_data/benchmarks/viscoelastic/results/'
 files = scan_dir(path)
-
-rank = 'rank[0]'
 files = [i for i in files if rank in i]
-
 x1, y1 = extract_xy(files, path)
+
+path = '/data/cx2_data/benchmarks/acoustic/results/'
+files = scan_dir(path)
+files = [i for i in files if rank in i]
+x2, y2 = extract_xy(files, path)
+
+path = '/data/cx2_data/benchmarks/tti/results/'
+files = scan_dir(path)
+files = [i for i in files if rank in i]
+x3, y3 = extract_xy(files, path)
+
 
 figname = 'test'
 
 with LinePlotter(figname=figname, normalised=True) as plot:
         plot.add_line(x0, y0, label='Elastic')
-        plot.add_line(x1, y1, label='Viscolastic', style='r-')
+        plot.add_line(x1, y1, label='Acoustic', style='r-')
+        plot.add_line(x2, y2, label='ViscoElastic', style='m-')
+        plot.add_line(x3, y3, label='TTI', style='g-')
